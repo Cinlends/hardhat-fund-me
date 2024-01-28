3c8d035054c7e86024875b2d7d5434feaa0375fa
LawrenceN3lson <hsc1325@outlook.com>
2024-01-28 17:50:26 +0800

    Add deployments to .gitignore and update deploy scripts

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,17 +1,24 @@
 // helper-hardhat-config.js
 const networkConfig = {
-	31337: {
-		// localhost没有chainlink的喂价合约,我们需要模拟
-		name: "localhost",
-	},
-	11155111: {
-		name: "sepolia",
-		// 这里的priceFeedAddress是我们在sepolia网络上部署的喂价合约的地址
-		priceFeedAddress: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
-	},
+  31337: {
+    // localhost没有chainlink的喂价合约,我们需要模拟
+    name: "localhost",
+  },
+  11155111: {
+    name: "sepolia",
+    // 这里的priceFeedAddress是我们在sepolia网络上部署的喂价合约的地址
+    priceFeedAddress: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+  },
 };
+
+developmentChains = ["hardhat", "localhost"];
+const DECIMALS = 8;
+const INITIAL_PRICE = 200000000000;
 
 // 导出配置,networkConfig可以在其他文件中使用
 module.exports = {
-	networkConfig,
+  networkConfig,
+  developmentChains,
+  DECIMALS,
+  INITIAL_PRICE,
 };
